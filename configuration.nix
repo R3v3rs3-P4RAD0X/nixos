1f396a2ff7118ef98594094f1f5543df9478795e
R3V3RS3_P4RAD0X <d.ellis01.dje@gmail.com>
2024-11-10 13:06:17 +0100

    Update NixOS configuration after successful rebuild

--- configuration.nix
+++ configuration.nix
@@ -30,8 +30,8 @@
     package = pkgs.linuxPackages.nvidiaPackages.stable;
   };
 
-  # services.displayManager.sddm.enable = true;
-  # services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   # services.desktopManager.plasma6.enable = true;
   # services.displayManager.defaultSession = "plasma";
 
