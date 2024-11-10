deb00d3a9db24e6870430aa80c09e27a5ad92f58
R3V3RS3_P4RAD0X <d.ellis01.dje@gmail.com>
2024-11-10 21:37:07 +0000

    Update NixOS configuration after successful rebuild

--- desktops/hyprland.nix
+++ desktops/hyprland.nix
@@ -5,6 +5,13 @@
 		enable = true;
 		xwayland.enable = true;
 	};
+	
+	services.xserver = {
+		displayManager = {
+			sddm.enable = true;
+		};
+	};
+	services.displayManager.sddm.wayland.enable = true;
 
 	security.polkit.enable = true;
 }
